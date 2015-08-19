37065463167a9478b5f6a2d2e82b4a1a23a13482
Jana Dvorakova <jana.dvorakova@kraxnet.cz>
2015-08-19 16:09:25 +0200

    rails 4 fix

--- lib/juggernaut_plugin/railtie.rb
+++ lib/juggernaut_plugin/railtie.rb
@@ -7,8 +7,6 @@ module JuggernautPlugin
 
       ActionView::Base.send(:include, Juggernaut::JuggernautHelper)
 
-      ActionView::Helpers::AssetTagHelper.register_javascript_expansion :juggernaut => ['juggernaut/swfobject', 'juggernaut/juggernaut']
-
       ActionController::Base.class_eval do
         alias_method :render_without_juggernaut, :render
         include Juggernaut::RenderExtension
